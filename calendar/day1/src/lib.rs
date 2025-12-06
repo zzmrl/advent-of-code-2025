0e3c0998ab9d8bb330ca60b111157a07d919139c
Zach Riel <zmriel@gmail.com>
2025-12-06 18:59:10 -0500

    feat: day 1 part 1

--- calendar/day1/src/lib.rs
+++ calendar/day1/src/lib.rs
@@ -1,45 +1,62 @@
-pub mod dial;
-
-fn parse_input(input: &str) -> Vec<i32> {
-    input
-        .lines()
-        .map(|line| {
-            let (direction, distance) = line.split_at(1);
-            let distance: i32 = distance.parse().unwrap();
-            match direction {
-                "L" => -distance,
-                "R" => distance,
-                _ => panic!("Invalid direction"),
-            }
-        })
-        .collect()
-}
-
 pub fn rotate_left(pos: u32, steps: u32) -> u32 {
-    100 + pos - steps % 100
+    let steps = steps % 100;
+    if steps > pos {
+        100 - (steps - pos)
+    } else {
+        pos - steps
+    }
 }
 
-// fn count_zeroes(input: &str) -> i32 {
-//     let mut pos = 0u8;
+pub fn rotate_right(pos: u32, steps: u32) -> u32 {
+    let steps = steps % 100;
+    if steps >= 100 - pos {
+        steps - (100 - pos)
+    } else {
+        pos + steps
+    }
+}
 
-//     for line in input.trim().lines() {
-//         let (direction, distance) = line.split_at(1);
-//         let distance: i32 = distance.parse().unwrap();
-//         match direction {
-//             "L" => -distance,
-//             "R" => distance,
-//             _ => panic!("Invalid direction"),
-//         }
-//     }
-//     0
-// }
+pub fn analyze_password(input: &str) -> u32 {
+    let mut zero_count = 0u32;
+    let mut pos = 50;
+    for line in input.lines() {
+        let (direction, distance) = line.split_at(1);
+        let distance: u32 = distance.parse().unwrap();
+        match direction {
+            "L" => pos = rotate_left(pos, distance),
+            "R" => pos = rotate_right(pos, distance),
+            _ => panic!("Invalid direction"),
+        }
+        if pos == 0 {
+            zero_count += 1;
+        }
+    }
+    zero_count
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const EXAMPLE_INPUT: &str = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82";
+
     #[test]
     fn test_rotate_left() {
-        assert_eq!(rotate_left(50, 68), 82)
+        assert_eq!(rotate_left(50, 68), 82);
+        assert_eq!(rotate_left(82, 30), 52);
+        assert_eq!(rotate_left(0, 5), 95);
+        assert_eq!(rotate_left(55, 55), 0);
+    }
+
+    #[test]
+    fn test_rotate_right() {
+        assert_eq!(rotate_right(52, 48), 0);
+        assert_eq!(rotate_right(95, 60), 55);
+        assert_eq!(rotate_right(0, 14), 14);
+    }
+
+    #[test]
+    fn test_analyze_password() {
+        assert_eq!(analyze_password(EXAMPLE_INPUT), 3);
     }
 }
--- calendar/day1/src/main.rs
+++ calendar/day1/src/main.rs
@@ -1,13 +1,12 @@
+use day1::analyze_password;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting...");
+    solve();
 }
 
-#[cfg(test)]
-mod tests {
-    const EXAMPLE_INPUT: &str = "L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82";
-
-    #[test]
-    fn test_main() {
-        assert_eq!(1, 1);
-    }
+fn solve() {
+    let input = include_str!("input");
+    let result = analyze_password(input);
+    println!("Solution: {result}");
 }
