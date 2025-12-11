6e5907f8abad36c9e710fd4cc0191e04a38787e2
Zach Riel <zmriel@gmail.com>
2025-12-11 17:27:28 -0500

    feat: day3 part2

--- calendar/day3/src/main.rs
+++ calendar/day3/src/main.rs
@@ -1,11 +1,13 @@
+use std::usize;
+
 fn main() {
     let input = include_str!("input");
     println!("Testing part one");
     let result = part_one(input);
     println!("Result: {result}");
-    // println!("Testing part two");
-    // let result2 = part_two(input);
-    // println!("Result: {result2}");
+    println!("Testing part two");
+    let result2 = part_two(input);
+    println!("Result: {result2}");
 }
 
 fn part_one(input: &str) -> u32 {
@@ -26,8 +28,25 @@ fn part_one(input: &str) -> u32 {
         .sum()
 }
 
-fn part_two(input: &str) -> u32 {
-    0
+fn part_two(input: &str) -> usize {
+    input
+        .lines()
+        .flat_map(|mut line| {
+            let find_max = |line: &str| {
+                line.chars().enumerate().fold(
+                    (0, '0'),
+                    |(j, nn), (i, n)| {
+                        if n > nn { (i, n) } else { (j, nn) }
+                    },
+                )
+            };
+            (0..12).rev().map(move |p| {
+                let (i, n) = find_max(&line[..line.len() - p]);
+                line = &line[i + 1..];
+                (n as u8 - b'0') as usize * 10usize.pow(p as u32)
+            })
+        })
+        .sum::<usize>()
 }
 
 #[cfg(test)]
@@ -44,8 +63,8 @@ mod tests {
         assert_eq!(part_one(EXAMPLE_INPUT), 357);
     }
 
-    // #[test]
-    // fn test_part_two() {
-    //     assert_eq!(part_two(EXAMPLE_INPUT), 2);
-    // }
+    #[test]
+    fn test_part_two() {
+        assert_eq!(part_two(EXAMPLE_INPUT), 3121910778619);
+    }
 }
