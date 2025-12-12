4b0b08ffcda57d989d55435daa48a6b70659d8d3
Zach Riel <zmriel@gmail.com>
2025-12-12 17:07:31 -0500

    feat: day4 part1

--- calendar/day4/src/main.rs
+++ calendar/day4/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = include_str!("input");
     println!("Testing part one");
@@ -8,8 +10,50 @@ fn main() {
     println!("Result: {result2}");
 }
 
+const DELTAS: [(i16, i16); 8] = [
+    (-1, 0),
+    (1, 0),
+    (0, -1),
+    (0, 1),
+    (-1, -1),
+    (-1, 1),
+    (1, -1),
+    (1, 1),
+];
+
 fn part_one(input: &str) -> u32 {
-    !todo!("Implement part one")
+    let map: HashMap<(i16, i16), u8> = input
+        .lines()
+        .enumerate()
+        .flat_map(|(row, line)| {
+            line.bytes().enumerate().map(move |(col, v)| {
+                let key = (row as i16, col as i16);
+                let value = match v {
+                    b'.' => 0,
+                    b'@' => 1,
+                    _ => panic!("Invalid input"),
+                };
+                (key, value)
+            })
+        })
+        .collect();
+
+    map.iter()
+        .filter(|(_key, value)| **value == 1)
+        .map(|((row, col), _value)| {
+            let neighbors: u8 = DELTAS
+                .iter()
+                .map(|(dr, dc)| {
+                    if let Some(&neighbor) = map.get(&(*row + dr, *col + dc)) {
+                        neighbor
+                    } else {
+                        0
+                    }
+                })
+                .sum();
+            if neighbors < 4 { 1 } else { 0 }
+        })
+        .sum()
 }
 
 fn part_two(input: &str) -> u32 {
@@ -34,7 +78,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(EXAMPLE_INPUT);
-        assert_eq!(result, 0);
+        assert_eq!(result, 13);
     }
 
     #[test]
